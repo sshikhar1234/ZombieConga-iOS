ada031424d466efd86f4897ead8a66c45c4dd0cb
sshikhar1234 <sshikharshah@gmail.com>
2019-10-16 20:20:03 -0400

    Added player, enemy and randomly spawning cats with score and lives update.

--- ZombieConga-iOS/GameScene.swift
+++ ZombieConga-iOS/GameScene.swift
@@ -1,89 +1,150 @@
 //
 //  GameScene.swift
-//  ZombieConga-iOS
+//  ZombieConga
 //
-//  Created by Shikhar Shah on 2019-10-16.
-//  Copyright © 2019 Lambton. All rights reserved.
+//  Created by Parrot on 2019-01-29.
+//  Copyright © 2019 Parrot. All rights reserved.
 //
 
 import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
-    
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
-    
+
+    var bg: SKSpriteNode!
+    var zombie: SKSpriteNode!
+    var grandMa: SKSpriteNode!
+    var cat: SKSpriteNode!
+    var livesLable: SKLabelNode!
+    var scoreLable: SKLabelNode!
+    var mouseX:CGFloat = 0
+    var mouseY:CGFloat = 0
+    var delay: Int = 0
+    var cats:[SKSpriteNode] = []
+
+    var lives = 500;
+    var score = 0;
     override func didMove(to view: SKView) {
+        //1. Set the background color of the app
+        self.backgroundColor = SKColor.black;
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
+                
+        //2. Setup Zombie
+        self.zombie = SKSpriteNode(imageNamed: "zombie1")
+        self.zombie.position = CGPoint(x:1000,y:1000)
+        addChild(self.zombie)
+        
+        //3. Setup Grandma
+        self.grandMa = SKSpriteNode(imageNamed: "enemy")
+        self.grandMa.position = CGPoint(x:size.width-100,y:size.height/2)
+        addChild(self.grandMa)
+        
+        //4. Setup Lives Label
+        self.livesLable = SKLabelNode(text: "Lives Remaining: \(lives)")
+        self.livesLable.fontSize = 65
+        self.livesLable.color = UIColor.orange
+        self.livesLable.position = CGPoint(x:400,y:size.height-300)
+        addChild(livesLable)
+
+        self.scoreLable = SKLabelNode(text: "Score: \(self.score)")
+        self.scoreLable.fontSize = 65
+        self.scoreLable.color = UIColor.orange
+        self.scoreLable.position = CGPoint(x:400,y:size.height-400)
+        addChild(scoreLable)
+
+        //5. Make the animation for grandma's movement
+        let   move1 = SKAction.move(to: CGPoint(x:size.width/2,y: 360), duration: 2)
+        let   move2 = SKAction.move(to: CGPoint(x:100,y: size.height/2), duration: 2)
+        let   move3 = SKAction.move(to: CGPoint(x:size.width/2,y: 360), duration: 2)
+        let   move4 = SKAction.move(to: CGPoint(x:size.width-100,y: size.height/2), duration: 2)
+        let grandMaAnimation = SKAction.sequence([move1,move2,move3,move4])
+        let grandMaLoop = SKAction.repeatForever(grandMaAnimation)
+        grandMa.run(grandMaLoop)
+    }
+
+    override func update(_ currentTime: TimeInterval){
+        if(currentTime.ulp == 1){
+            print("Wind it up")
+        }
+        //1. Increase the delay/counter
+        self.delay = self.delay + 1
+        //2. Move zombie to mouse tap point
+        moveZombieToPoint(x: self.mouseX, y: self.mouseY)
+        
+        //3. Detect Collision
+        if(self.zombie.frame.intersects(self.grandMa.frame)){
+            print("Collision occured")
+            if(self.lives>0){
+            //4. Decrease lives by one
+            self.lives = self.lives - 1
+            livesLable.text = "Lives Remaining: \(lives)"
+            }
+        }
+        //5. Spawn Random Cats every 100th frame
+        if(delay%70 == 0){
+            spawnCat()
         }
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
+        //6. Detect Cat and Zombie Collision
+        for cat in cats {
+            if(cat.frame.intersects(self.zombie.frame)){
+                //Collision between zombie and one cat
+                
+                //1. Increase Score
+                self.score = self.score + 10
+                scoreLable.text = "Score: \(score)"
+
+                //2. Remove cat from screen
+                cat.removeFromParent()
+                //3. Remove cat from Array
+                let index = cats.index(of: cat)
+                cats.remove(at: index!)
+            }
             
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
-    }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
         }
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
     }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+
+    func moveZombieToPoint(x: CGFloat,y: CGFloat){
+        let a = (self.mouseX - self.zombie.position.x);
+        let b = (self.mouseY - self.zombie.position.y)
+        let distance = sqrt((a*a)+(b*b))
+        let xn = (a/distance)
+        let yn = (b/distance)
+        self.zombie.position.x = self.zombie.position.x + (xn*10);
+        self.zombie.position.y = self.zombie.position.y + (yn*10);
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+        var locationTouched = touches.first
+        if(locationTouched == nil)        {
+            return
+        }
+        let mousePosition = locationTouched!.location(in: self)
+        print("MOUSE X : \(mousePosition.x)")
+        print("MOUSE Y : \(mousePosition.y)")
+        self.mouseX = mousePosition.x
+        self.mouseY = mousePosition.y
     }
+
+      
+       
+         
+        func spawnCat() {
+            // Add a cat to a static location
+            let cat = SKSpriteNode(imageNamed: "cat")
     
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
+            // generate a random x position
+            let randomXPos = Int.random(in: 00 ... 1800)
+            let randomYPos = Int.random(in: 00 ... 1800)
+            cat.position = CGPoint(x:randomXPos, y:randomYPos)
     
+            // add the cat to the screen
+            addChild(cat)
     
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
-    }
+            // add the cat to the array
+            self.cats.append(cat)
+    
+        }
+        
 }
--- ZombieConga-iOS/GameViewController.swift
+++ ZombieConga-iOS/GameViewController.swift
@@ -1,9 +1,9 @@
 //
 //  GameViewController.swift
-//  ZombieConga-iOS
+//  ZombieConga
 //
-//  Created by Shikhar Shah on 2019-10-16.
-//  Copyright © 2019 Lambton. All rights reserved.
+//  Created by Parrot on 2019-01-29.
+//  Copyright © 2019 Parrot. All rights reserved.
 //
 
 import UIKit
@@ -14,36 +14,17 @@ class GameViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+      
+        let scene = GameScene(size:CGSize(width:2048, height:1536))
+        let skView = self.view as! SKView
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+        skView.ignoresSiblingOrder = true
+        scene.scaleMode = .aspectFill
+        skView.presentScene(scene)
         
-        if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene)
-            }
-            
-            view.ignoresSiblingOrder = true
-            
-            view.showsFPS = true
-            view.showsNodeCount = true
-        }
+        
     }
-
-    override var shouldAutorotate: Bool {
-        return true
-    }
-
-    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
-        } else {
-            return .all
-        }
-    }
-
     override var prefersStatusBarHidden: Bool {
         return true
     }
